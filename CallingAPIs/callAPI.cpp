84c4c659fb36cefc5db782b0979e7ac3b7c3d061
FrozeyNight <frozeynight@gmail.com>
2025-10-11 19:10:59 +0200

    Made the program display the current temperature at set coordinates using the open-meteo.com weather API

--- CallingAPIs/callAPI.cpp
+++ CallingAPIs/callAPI.cpp
@@ -18,7 +18,11 @@ int main(){
     curl = curl_easy_init();
 
     if(curl){
-        curl_easy_setopt(curl, CURLOPT_URL, "https://api.chucknorris.io/jokes/random");       
+        struct curl_slist* headers = NULL;
+        headers = curl_slist_append(headers, "Accept: application/json");
+
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.open-meteo.com/v1/forecast?timezone=Europe%2FBerlin&latitude=24.62&longitude=16.65&current=temperature_2m");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
@@ -26,7 +30,7 @@ int main(){
         response = curl_easy_perform(curl);
 
         if(response == CURLE_OK){
-            readBuffer.erase(0, readBuffer.find("value") + 8);
+            readBuffer.erase(0, readBuffer.find("900") + 21);
             readBuffer.erase(readBuffer.length() - 2, readBuffer.length());
 
             std::cout << readBuffer << "\n";
@@ -35,6 +39,7 @@ int main(){
             std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(response) << "\n";
         }
 
+        curl_slist_free_all(headers);
         curl_easy_cleanup(curl);
     }
 
