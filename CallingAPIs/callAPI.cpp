c3920040d64a5ee7a2eccb6770d4cd0d96a0b669
FrozeyNight <frozeynight@gmail.com>
2025-10-12 22:31:20 +0200

    Added validation of latitude and longitude user input

--- CallingAPIs/callAPI.cpp
+++ CallingAPIs/callAPI.cpp
@@ -18,10 +18,34 @@ int main(){
     curl = curl_easy_init();
 
     if(curl){
+
+        std::string apiAddress = "https://api.open-meteo.com/v1/forecast?timezone=Europe%2FBerlin&latitude=";
+
+        double latitude = 0;
+        double longitude = 0;
+
+        do{
+            std::cout << "Latitude: ";
+            std::cin >> latitude;
+            if(latitude <= 90 && latitude >= -90)
+                break;
+            std::cout << "The latitude must be between -90 and 90\n";
+        }while(true);
+
+        do{
+            std::cout << "Longitude: ";
+            std::cin >> longitude;
+            if(longitude <= 180 && longitude >= -180)
+                break;
+            std::cout << "The longitude must be between -180 and 180\n";
+        }while(true);
+
+        apiAddress = apiAddress + std::to_string(latitude) + "&longitude=" + std::to_string(longitude) + "&current=temperature_2m";
+
         struct curl_slist* headers = NULL;
         headers = curl_slist_append(headers, "Accept: application/json");
 
-        curl_easy_setopt(curl, CURLOPT_URL, "https://api.open-meteo.com/v1/forecast?timezone=Europe%2FBerlin&latitude=24.62&longitude=16.65&current=temperature_2m");
+        curl_easy_setopt(curl, CURLOPT_URL, apiAddress.c_str()); // curl expects is more for C, so it doesn't know what a std::string is. You have to convert it into a char*
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
@@ -33,7 +57,7 @@ int main(){
             readBuffer.erase(0, readBuffer.find("900") + 21);
             readBuffer.erase(readBuffer.length() - 2, readBuffer.length());
 
-            std::cout << readBuffer << "\n";
+            std::cout << "The temperature at " << latitude << " " << longitude << " is " << readBuffer << "c" << "\n";
         }
         else{
             std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(response) << "\n";
